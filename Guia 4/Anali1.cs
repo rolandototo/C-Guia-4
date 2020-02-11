aba8fa8d038c16fb5ff1324073acb917e8c668c6
Rolando Escobar <rolandototo@gmail.com>
2020-02-10 20:28:29 -0600

    Agrergo Analisis 1 y empieso investigacion 1

--- Guia 4/Anali1.cs
+++ Guia 4/Anali1.cs
@@ -8,5 +8,64 @@ namespace Guia_4
 {
     class Anali1
     {
+        public Anali1()
+        {
+            double preguntas, respuestas, porcentage;
+            inicio();
+            filtro();
+            Console.ReadKey();
+
+
+            void inicio()
+            {
+                Console.WriteLine("Programa para saber el nivel de aprobacion de respuestas.");
+                Console.WriteLine("\nIngrese La  cantidad de preguntas en numeros que le realizaron.");
+                preguntas = double.Parse(Console.ReadLine());
+                Console.WriteLine("\nIngrese La  cantidad de preguntas que contesto correctamente.");
+                respuestas = double.Parse(Console.ReadLine());
+
+                porcentage = (respuestas / preguntas)* 100;
+            }
+
+            void filtro()
+            {
+                if (porcentage >= 90)
+                {
+                    Console.WriteLine("\nSe encuentra en el nivel maximo con {0}.", porcentage);
+
+                }
+
+                if (porcentage >= 75 && porcentage < 90)
+                {
+                    Console.WriteLine("\nSe encuentra en el nivel medio con {0}.", porcentage);
+
+                }
+
+                if (porcentage >= 50 && porcentage < 75)
+                {
+                    Console.WriteLine("\nSe encuentra en el nivel regular con {0}.", porcentage);
+
+                }
+
+                if (porcentage < 50)
+                {
+                    Console.WriteLine("\nSe encuentra Fuera de nivel con {0}.", porcentage);
+
+
+                }
+                else
+                {
+                    Console.WriteLine("\nIngrese datos validos para poder hacer la operacion.");
+                    inicio();
+                    filtro();
+                    Console.ReadKey();
+                }
+            }
+
+            
+
+
+
+        }
     }
 }
--- Guia 4/Investi1.cs
+++ Guia 4/Investi1.cs
@@ -8,6 +8,40 @@ namespace Guia_4
 {
     class Investi1
     {
-        
+        public Investi1()
+        {
+            //Dise;ar un progrma que nos permita saber cual es el mayor entre 2 numeros
+
+            Console.WriteLine("Dise;ar un progrma que nos permita saber cual es el mayor entre 2 numeros");
+            Console.WriteLine("\n");
+
+            Console.WriteLine("El mayor entre 2 numeros");
+            Console.WriteLine("\n");
+
+            int a, b;
+
+            Console.WriteLine("Escribe el primer numero");
+            a = int.Parse(Console.ReadLine());
+
+
+            Console.WriteLine("Escribe el segundo numero");
+            b = int.Parse(Console.ReadLine());
+
+            if (a > b)
+            {
+                Console.WriteLine("El numero {0} es mayor que {1}", a, b);
+            }
+            else if (a == b)
+            {
+                Console.WriteLine("El numero {0} es igual que {1}", a, b);
+            }
+            else
+            {
+                Console.WriteLine("El numero {1} es mayor que {0}", a, b);
+            }
+            Console.WriteLine("\n");
+            Console.WriteLine("Apreta cualquier tecla para cerrar");
+            Console.ReadKey();
+        }
     }
 }

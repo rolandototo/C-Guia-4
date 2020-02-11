a47336b466fa10063475497115ad9a4caf17b158
Rolando Escobar <rolandototo@gmail.com>
2020-02-10 21:00:34 -0600

    Agrado Investi2

--- Guia 4/Investi2.cs
+++ Guia 4/Investi2.cs
@@ -8,5 +8,75 @@ namespace Guia_4
 {
     class Investi2
     {
+        public Investi2()
+        {
+            //Dise;ar un progrma que nos permita saber cual es el menor entre 3 numeros
+
+            Console.WriteLine("Dise;ar un progrma que nos permita saber cual es el menor entre 3 numeros");
+            Console.WriteLine("\n");
+
+            Console.WriteLine("El menor entre 3 numeros");
+            Console.WriteLine("\n");
+
+            double a, b, c;
+
+            Console.WriteLine("Escribe el primer numero");
+            a = double.Parse(Console.ReadLine());
+
+
+            Console.WriteLine("Escribe el segundo numero");
+            b = double.Parse(Console.ReadLine());
+
+
+            Console.WriteLine("Escribe el tercer numero");
+            c = double.Parse(Console.ReadLine());
+
+            if (a < b && a < c)
+            {
+                Console.WriteLine("El numero {0} es el menor", a);
+                if (b < c)
+                {
+                    Console.WriteLine("El numero {0} es el de enmedio", b);  
+                    Console.WriteLine("El numero {0} es el mayor", c);
+                }
+                else
+                {
+                    Console.WriteLine("El numero {0} es el de enmedio", c);
+                    Console.WriteLine("El numero {0} es el mayor", b);
+                }
+            }
+            else if (b < a && b < c)
+            {
+                Console.WriteLine("El numero {0} es el menor", b);
+                if (a < c)
+                {
+                    Console.WriteLine("El numero {0} es el de enmedio", a);
+                    Console.WriteLine("El numero {0} es el mayor", c);
+                }
+                else
+                {
+                    Console.WriteLine("El numero {0} es el de enmedio", c);
+                    Console.WriteLine("El numero {0} es el mayor", a);
+                }
+            }
+            else if (c < a && c < b)
+            {
+                Console.WriteLine("El numero {0} es el menor", c);
+                if (b < a)
+                {
+                    Console.WriteLine("El numero {0} es el de enmedio", b);
+                    Console.WriteLine("El numero {0} es el mayor", a);
+                }
+                else
+                {
+                    Console.WriteLine("El numero {0} es el de enmedio", a);
+                    Console.WriteLine("El numero {0} es el mayor", b);
+                }
+            }
+
+            Console.WriteLine("\n");
+            Console.WriteLine("Apreta cualquier tecla para cerrar");
+            Console.ReadKey();
+        }
     }
 }
